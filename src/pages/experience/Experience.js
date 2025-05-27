3f8d33dffcd7fd6f56d4aeafc9f84763cb60642f
Adithya441 <129399585+Adithya441@users.noreply.github.com>
2025-05-27 07:35:58 +0530

    Update Experience.js

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/header/Header";
-import Footer from "../../components/footer/Footer";
+//import Footer from "../../components/footer/Footer";
 import ExperienceAccordion from "../../containers/experienceAccordion/ExperienceAccordion.js";
 import "./Experience.css";
 import { experience } from "../../portfolio.js";
